8d0198a0224d3c081e6b741a0764ea656ea26074
Kavindu Methmal <136598207+kmigalavithana@users.noreply.github.com>
2025-02-23 20:19:40 +0530

    Install prettier and creat ui for task add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-
+import {useState} from "react";
 
 const App = () => {
-
+    const [task, setTask] = useState('')
+    const handleInput = (event) => {
+        setTask(event.target.value)
+    }
   return (
-    <>
-     <h1>React Do To App</h1>
-    </>
-  )
-}
+   <>
+       <div>
+           <label
+               htmlFor="small-input"
+               className="mb-2 text-sm font-medium text-dark"
+           >
+               Task
+           </label>
+           <input
+               type="text"
+               id="small-input"
+               name="Task"
+               onChange={handleInput}
+               className="w-1/2 m-4 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs "
+           />
+           <button
+               type="button"
+               className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+           >
+               Add new task
+           </button>
 
-export default App
+       </div>
+       <div>
+           {task}
+       </div>
+   </>
+  );
+};
+
+export default App;
