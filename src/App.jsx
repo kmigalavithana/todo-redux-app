b74901e7f3a6200c344d0e5eb55dedf2b99e09c3
Kavindu Methmal <136598207+kmigalavithana@users.noreply.github.com>
2025-02-25 16:32:55 +0530

    Create object and assinged singl task into that object with id

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {storeTask} from "./utilites/state/taskSlice.js";
 
 const App = () => {
   const [task, setTask] = useState("");
   const dispatch = useDispatch();
+  const alreadyAddTask = useSelector(state => state.alreadyAddedTask.tasks);
   const handleInput = (event) => {
     setTask(event.target.value);
   };
   const addNewTask = () => {
     dispatch(storeTask(task));
+
+    setTask("");
   };
 
   return (
@@ -25,6 +28,7 @@ const App = () => {
           type="text"
           id="small-input"
           name="Task"
+          value={task}
           onChange={handleInput}
           className="w-1/2 m-4 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs "
         />
@@ -36,7 +40,14 @@ const App = () => {
           Add new task
         </button>
       </div>
-
+      <div>
+        {alreadyAddTask.length > 0 && alreadyAddTask.map((task) => (
+            <div key={task.id} className='m-4'>
+              <p className='inline mr-4'> {task.task}</p>
+              <button className= "text-white bg-red-500 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Remove</button>
+            </div>
+        ))}
+      </div>
     </>
   );
 };
